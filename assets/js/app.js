719c5ac76e6c9a82cffa6ac4efaf71ff701a5ee3
azimjon <abozorov0303@gmail.com>
2024-01-30 14:46:54 +0300

    hello

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+// const btnEl = document.querySelector(".btn");
+// const h1El = document.querySelector(".text");
+// let nameArr = ["azimjon","Muhammadali","murod","shaxriyor","davlatbek",""]
+// btnEl.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     let nameIndex = Math.floor(Math.random()*nameArr.length);
+//     h1El.innerHTML = nameArr[nameIndex]
+
+// })
